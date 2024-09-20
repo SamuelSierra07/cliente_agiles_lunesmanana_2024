509483377afdeb1cb4a87ae0b98cd15443553f83
jsantiar <juliansantiago2307@gmail.com>
2024-09-20 07:24:26 -0500

    se crea archivo para simular base de datos

---
+++ src/controllers/BaseDatos.js
@@ -0,0 +1,17 @@
+let gastos=Array(
+    {
+        id: 1,
+        descripcion: "Pasaje de sabaneta a cesde",
+        monto: 8000,
+        fecha:"2024-9-20"
+    },
+    {
+        id: 2,
+        descripcion: "Empanada de pollo  + coca cola ",
+        monto: 10000,
+        fecha:"2024-6-20"
+    }
+
+    
+
+)
